c71e06ab2941fe77ce0fb998fda7a57b9ad155ca
scheldejonas <scheldejonas@gmail.com>
2016-05-08 13:07:18 +0200

    Upgrade - Stock Quote PriceStragegy done 80%

--- src/model/drug/pricestrategy/StockQuote.java
+++ src/model/drug/pricestrategy/StockQuote.java
@@ -13,34 +13,65 @@ import java.util.ArrayList;
  */
 public class StockQuote implements DrugPriceModelInterface {
     
-    private StockFinder stockHandler;
-    private ArrayList<String> stockShortNames;
+    private StockFinder excelReader;
+    private ArrayList<String> stockShortNamesPrices;
     private ArrayList<Double> recievedStockPrices;
+    private ArrayList<String> stockShortNamesAmount;
+    private ArrayList<Double> recievedStockAmount;
 
     public StockQuote() {
-        stockShortNames = new ArrayList();
-        stockShortNames.add("ACUR");
-        stockShortNames.add("ADMP");
-        stockShortNames.add("AEY");
-        stockShortNames.add("BV");
-        stockShortNames.add("CSQ");
-        stockShortNames.add("CALM");
-        stockShortNames.add("CAMP");
-        stockShortNames.add("CASH");
-        stockShortNames.add("MCBI");
-        stockShortNames.add("MGCD");
-        stockShortNames.add("MFI");
-        stockShortNames.add("MU");
-        stockShortNames.add("MSFT");
-        stockShortNames.add("PME");
-        stockShortNames.add("PZZI");
-        stockShortNames.add("PSTI");
-        stockShortNames.add("SAFT");
-        stockShortNames.add("SGNT");
-        stockShortNames.add("SLXP");
-        stockShortNames.add("GCVRZ");
-        this.stockHandler = new StockFinder();
-        recievedStockPrices = stockHandler.getStockData(stockShortNames);
+        stockShortNamesPrices = new ArrayList();
+    }
+    
+    public void initPriceList() {
+        stockShortNamesPrices.add("ACUR");
+        stockShortNamesPrices.add("ADMP");
+        stockShortNamesPrices.add("AEY");
+        stockShortNamesPrices.add("BV");
+        stockShortNamesPrices.add("CSQ");
+        stockShortNamesPrices.add("CALM");
+        stockShortNamesPrices.add("CAMP");
+        stockShortNamesPrices.add("CASH");
+        stockShortNamesPrices.add("MCBI");
+        stockShortNamesPrices.add("MGCD");
+        stockShortNamesPrices.add("MFI");
+        stockShortNamesPrices.add("MU");
+        stockShortNamesPrices.add("MSFT");
+        stockShortNamesPrices.add("PME");
+        stockShortNamesPrices.add("PZZI");
+        stockShortNamesPrices.add("PSTI");
+        stockShortNamesPrices.add("SAFT");
+        stockShortNamesPrices.add("SGNT");
+        stockShortNamesPrices.add("SLXP");
+        stockShortNamesPrices.add("GCVRZ");
+        this.excelReader = new StockFinder();
+        recievedStockPrices = excelReader.getStockData(stockShortNamesPrices);
+    }
+    
+    public void initAmountList() {
+        stockShortNamesAmount.add("MPWR");
+        stockShortNamesAmount.add("TYPE");
+        stockShortNamesAmount.add("MRCC");
+        stockShortNamesAmount.add("MONT");
+        stockShortNamesAmount.add("OSBC");
+        stockShortNamesAmount.add("OXLC");
+        stockShortNamesAmount.add("OXGN");
+        stockShortNamesAmount.add("PCAR");
+        stockShortNamesAmount.add("PCO");
+        stockShortNamesAmount.add("ACUR");
+        stockShortNamesAmount.add("PDLI");
+        stockShortNamesAmount.add("PENN");
+        stockShortNamesAmount.add("PELT");
+        stockShortNamesAmount.add("PEOP");
+        stockShortNamesAmount.add("PFBX");
+        stockShortNamesAmount.add("PBCT");
+        stockShortNamesAmount.add("PWRD");
+        stockShortNamesAmount.add("SEAC");
+        stockShortNamesAmount.add("STX");
+        stockShortNamesAmount.add("SMTC");
+        
+        this.excelReader = new StockFinder();
+        recievedStockAmount = excelReader.getStockData(stockShortNamesAmount);
     }
 
 
